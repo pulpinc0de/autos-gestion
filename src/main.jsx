89306e77dd2fdfcf74cfc251e4c382eb7b92fb10
Maribel Pereyra <mrblpryra@gmail.com>
2025-11-18 00:02:01 -0300

    Subida inicial del proyecto autos-gestion

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import "./styles/authBackground.css";
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import './styles/authBackground.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/fron-autos">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
